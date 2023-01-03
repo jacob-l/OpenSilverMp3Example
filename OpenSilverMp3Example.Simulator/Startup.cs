2044e349e7fe29333c00b90c26e27d6802501a5d
Iakov Lilo <jlilo@hwdtech.com>
2023-01-03 12:29:15 +1100

    Add project files.

---
+++ OpenSilverMp3Example.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace OpenSilverMp3Example.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
---
+++ OpenSilverMp3Example/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverMp3Example
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+
+            Uri beepUri = new Uri("/OpenSilverMp3Example;component/Media/loaded.mp3", UriKind.Relative);
+            BeeperLoaded.Source = beepUri;
+        }
+
+        void ButtonToPlayAudio_Click(object sender, RoutedEventArgs e)
+        {
+            BeeperLoaded.Play();
+        }
+
+        void ButtonToPauseAudio_Click(object sender, RoutedEventArgs e)
+        {
+            BeeperLoaded.Pause();
+        }
+    }
+}
